5be3a503068778157785018045d4bb39f637208c
Razmoth <Razmoth@RAZMOTH-PC>
2026-08-26 23:20:26 +0400

    Add project files.

---
+++ SGREDecryptor/MT19937.cs
@@ -0,0 +1,94 @@
+﻿// C# port of https://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/MT2002/CODES/mt19937ar.c
+
+namespace SGREDecryptor;
+
+public class MT19937
+{
+    private const int N = 624;
+    private const int M = 397;
+    private const uint MATRIX_A = 0x9908B0DF;
+    private const uint UPPER_MASK = 0x80000000;
+    private const uint LOWER_MASK = 0X7FFFFFFF;
+
+    private readonly uint[] mt = new uint[N + 1];
+    private int mti = N + 1;
+
+    public MT19937(uint seed)
+    {
+        Init(seed);
+    }
+
+    public MT19937(ReadOnlySpan<uint> seed)
+    {
+        int i = 1, j = 0, k;
+
+        Init(0x12BD6AA);
+
+        for (k = Math.Max(seed.Length, N) ; k > 0; k--)
+        {
+            mt[i] = (uint)((mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 0x19660D))
+              + seed[j] + j);
+            i++; j++;
+            if (i >= N) { mt[0] = mt[N - 1]; i = 1; }
+            if (j >= seed.Length) j = 0;
+        }
+
+        for (k = N - 1; k > 0; k--)
+        {
+            mt[i] = (uint)((mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 0x5D588B65)) - i);
+            i++;
+            if (i >= N) { mt[0] = mt[N - 1]; i = 1; }
+        }
+
+        mt[0] = 0x80000000;
+    }
+
+    private void Init(uint seed)
+    {
+        mt[0] = seed;
+        for (mti = 1; mti < N; mti++)
+        {
+            mt[mti] = (uint)(0x6C078965 * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti);
+        }
+    }
+
+    public uint UInt32()
+    {
+        uint y;
+        Span<uint> mag01 = [ 0, MATRIX_A ];
+
+        if (mti >= N)
+        {
+            int kk;
+
+            if (mti == N + 1)
+            {
+                Init(0x1571);
+            }
+            
+            for (kk = 0; kk < (N - M); kk++)
+            {
+                y = mt[kk] & UPPER_MASK | mt[kk + 1] & LOWER_MASK;
+                mt[kk] = mt[kk + M] ^ (y >> 1) ^ mag01[(int)(y & 1)];
+            }
+
+            for (; kk < N - 1; kk++)
+            {
+                y = mt[kk] & UPPER_MASK | mt[kk + 1] & LOWER_MASK;
+                mt[kk] = mt[kk + (M - N)] ^ (y >> 1) ^ mag01[(int)(y & 0x1)];
+            }
+
+            y = mt[N - 1] & UPPER_MASK | mt[0] & LOWER_MASK;
+            mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ mag01[(int)(y & 0x1)];
+
+            mti = 0;
+        }
+
+        y = mt[mti++];
+        y ^= y >> 11;
+        y ^= (y << 7) & 0x9D2C5680;
+        y ^= (y << 15) & 0xEFC60000;
+        y ^= y >> 18;
+        return y;
+    }
+}
---
+++ SGREDecryptor/Program.cs
@@ -0,0 +1,152 @@
+﻿using SGREDecryptor;
+using System.CommandLine;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+Argument<DirectoryInfo> folderArgument = new("folder");
+
+folderArgument.AcceptLegalFilePathsOnly();
+
+RootCommand rootCommand = new("SGREDecryptor")
+{
+    folderArgument,
+};
+
+rootCommand.SetAction(Parse);
+return rootCommand.Parse(args).Invoke();
+
+void Parse(ParseResult result)
+{
+    DirectoryInfo dir = result.GetRequiredValue(folderArgument);
+
+    Main(dir);
+}
+
+void Main(DirectoryInfo dir)
+{
+    foreach (FileInfo file in dir.EnumerateFiles("*.m"))
+    {
+        try
+        {
+            Decrypt(file);
+            Console.WriteLine($"Decrypted {file.Name} !!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unable to decrypt {file.Name}: {e.Message}");
+        }
+    }
+}
+
+void Decrypt(FileInfo file)
+{
+    Console.WriteLine($"File: {file.FullName}");
+
+    if (file.Length < 8)
+    {
+        throw new InvalidDataException("File too small !!");
+    }
+
+    using Stream stream = file.OpenRead();
+    using BinaryReader reader = new(stream);
+
+    int signature = reader.ReadInt32();
+    Console.WriteLine($"Signature: 0x{signature:X8}");
+
+    if (signature != 0x737A6D) // mzs
+    {
+        throw new InvalidDataException("Not a valid mzs signature !!");
+    }
+
+    int decompressedSize = reader.ReadInt32();
+    Console.WriteLine($"DecompressedSize: 0x{decompressedSize:X8}");
+
+    long size = stream.Length - stream.Position;
+    Console.WriteLine($"Size: 0x{size:X16}");
+
+    Span<byte> seed = stackalloc byte[16];
+    ReadOnlySpan<byte> prefix = "Rk3nwA8ZYV0yV"u8;
+    ReadOnlySpan<byte> name = Encoding.UTF8.GetBytes(file.Name);
+
+    if (name.Length > 0x100)
+    {
+        throw new Exception("File name too long !!");
+    }
+
+    Span<byte> buffer = stackalloc byte[prefix.Length + file.Name.Length];
+
+    prefix.CopyTo(buffer);
+    name.CopyTo(buffer[prefix.Length..]);
+
+    int n = MD5.HashData(buffer, seed);
+    if (n != seed.Length)
+    {
+        throw new InvalidDataException("Seed size mismatch !!");
+    }
+
+    Console.WriteLine($"Seed: {Convert.ToHexString(seed)}");
+
+    ReadOnlySpan<uint> seedUints = MemoryMarshal.Cast<byte, uint>(seed);
+
+    MT19937 mt = new(seedUints);
+
+    int blockSize = 0x83;
+    Span<byte> xorpad = stackalloc byte[blockSize];
+    Span<byte> next = stackalloc byte[4];
+    Span<uint> nextUints = MemoryMarshal.Cast<byte, uint>(next);
+
+    for (int i = 0; i < xorpad.Length; i++)
+    {
+        if (i % next.Length == 0)
+        {
+            nextUints[0] = mt.UInt32();
+        }
+
+        xorpad[i] = next[i % next.Length];
+    }
+
+    using MemoryStream ms = new();
+
+    Span<byte> block = stackalloc byte[blockSize];
+
+    long remaining = size;
+    while (remaining > 0)
+    {
+        int count = (int)Math.Min(remaining, blockSize);
+
+        stream.ReadExactly(block[..count]);
+
+        for (int i = 0; i < count; i++)
+        {
+            block[i] ^= xorpad[i];
+        }
+
+        ms.Write(block[..count]);
+
+        remaining -= count;
+    }
+
+    ms.Position = 0;
+
+    using ZstandardStream zstdStream = new(ms, CompressionMode.Decompress);
+    using MemoryStream output = new();
+
+    zstdStream.CopyTo(output);
+
+    if (output.Length != decompressedSize)
+    {
+        throw new InvalidDataException("Decompressed size mismatch !!");
+    }
+
+    output.Position = 0;
+
+    if (file.Directory != null)
+    {
+        DirectoryInfo outputDir = file.Directory.CreateSubdirectory("decrypted");
+        string outputPath = Path.Combine(outputDir.FullName, file.Name);
+        using FileStream outFile = File.OpenWrite(outputPath);
+        output.CopyTo(outFile);
+    }
+}
